fc4bfd1c6b31d97388a0d60ba31bad61c7fa533e
Taylor Roberts <taylorlroberts7@gmail.com>
2021-11-02 16:35:05 -0600

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+const MarketingApp = App;
+
+//  If we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// If we are running through container, we should export the mount function
+export { mount, MarketingApp };
